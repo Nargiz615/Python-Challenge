cb14c44972546917d281a2ee9e0ede3482385899
Nargiz615 <shemssulldinnargiz@yahoo.com>
2023-09-25 23:21:31 -0500

    uploaded

--- PyPoll/Pypoll.py
+++ PyPoll/Pypoll.py
@@ -17,41 +17,91 @@ import os
 import csv
 
 csvpath = os.path.join('.', 'Resources', 'election_data.csv')
+##initialize counters and dictionaries, lists
+file_to_output = os.path.join("analysis", "election_analysis.txt")
 
+# Total Vote Counter
+total_votes = 0
 
+# Candidate Options and Vote Counters
+candidate_options = []
+candidate_votes = {}
 
-# Method 2: Improved Reading using CSV module
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
 
-with open(csvpath) as csvfile:
+# Read the csv and convert it into a list of dictionaries
+with open(csvpath) as election_data:
+    reader = csv.reader(election_data)
 
-    # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
+    # Read the header
+    header = next(reader)
 
-    total_votes= 0
-    total_candidates= []
-    x= {}
-    # Read the header row first (skip this step if there is now header)
-    csv_header = next(csvreader)
+    # For each row...
+    for row in reader:
 
-    # Read each row of data after the header
-    for row in csvreader:
-        total_votes= total_votes +1     
-        if row[2] not in total_candidates:
-            total_candidates.append(row[2]) 
-            x[row[2]]=0
-        x[row[2]]+=1
-print (x)
-print (total_candidates)
-print (total_votes)
-# =============================================================================
+        # Run the loader animation
+        print(". ", end=""),
 
-# 
-#     
-# #loop through csv for row in reader:
+        # Add to the total vote count
+        total_votes = total_votes + 1
 
-# 
-# a= candidates
-# 
-# = ["Charles Casper Stockham", "Diana DeGette", "Raymon Anthony Doane""]
-#          print(a)
-# =============================================================================
+        # Extract the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        # (In a way, our loop is "discovering" candidates as it goes)
+        if candidate_name not in candidate_options:
+
+            # Add it to the list of candidates in the running
+            candidate_options.append(candidate_name)
+
+            # And begin tracking that candidate's voter count
+            candidate_votes[candidate_name] = 0
+
+        # Then add a vote to that candidate's count
+        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+
+# Print the results and export the data to our text file
+with open(file_to_output, "w") as txt_file:
+
+    # Print the final vote count (to terminal)
+    election_results = (
+        f"\n\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+
+    # Save the final vote count to the text file
+    txt_file.write(election_results)
+
+    # Determine the winner by looping through the counts
+    for candidate in candidate_votes:
+
+        # Retrieve vote count and percentage
+        votes = candidate_votes.get(candidate)
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+        # Determine winning vote count and candidate
+        if (votes > winning_count):
+            winning_count = votes
+            winning_candidate = candidate
+
+        # Print each candidate's voter count and percentage (to terminal)
+        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
+        print(voter_output, end="")
+
+        # Save each candidate's voter count and percentage to text file
+        txt_file.write(voter_output)
+
+    # Print the winning candidate (to terminal)
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+
+    # Save the winning candidate's name to the text file
+    txt_file.write(winning_candidate_summary)
